de0010c70c7c06cf1ca177bbc987446299b81b33
code_commit-at-267692300788 <nikhil.kulthe@mitaoe.ac.in>
2024-09-06 10:04:22 +0530

    first commit

---
+++ javascript1.js
@@ -0,0 +1,22 @@
+// var a = 1923.81 ;// integer type 
+// var n = "nikhil@123"; // string type
+// var bl = true; // boolean type 
+// // var is a container stores data
+// // semicolon is not compulsory it doesnot gives error
+
+
+// int a = 3; // int datatype
+//     a = 50;
+//     a= 60;
+//     a= "nikhil"; // after updated string is also allowed in java script , it is not allowed in rule but the code is executed
+
+
+var py = 3.14;
+var radius = 5;
+
+var area = py * (radius*radius);
+
+console.log("The area is : " + area);
+
+document.write("The area is : " + area);
+
